6cbb2d1410aef4a47b2f410fbda32edd06578921
evan-colorkrew <evertjannes.degraaff@colorkrew.com>
2024-06-03 21:38:10 +0900

    chore: Add huggingface-cli login to setup script

--- docker/base/setup.sh
+++ docker/base/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+huggingface-cli login --token=${HF_TOKEN}
 mkdir /models
 git clone https://${HF_USERNAME}:${HF_TOKEN}@huggingface.co/mistralai/Mistral-7B-Instruct-v0.3 /models/Mistral-7B-Instruct-v0.3
 python3 /setup-mistral-instruct.py

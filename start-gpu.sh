47d0b90baa831353382606538e70ba104e26d831
evan-colorkrew <evertjannes.degraaff@colorkrew.com>
2024-06-03 17:25:31 +0900

    build container on up

--- start-gpu.sh
+++ start-gpu.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker compose -f ./docker/docker-compose.yml -f ./docker/docker-compose-gpu.yml up -d
+docker compose -f ./docker/docker-compose.yml -f ./docker/docker-compose-gpu.yml up -d --build
 docker compose -f ./docker/docker-compose.yml -f ./docker/docker-compose-gpu.yml exec base sh

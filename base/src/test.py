009d52cbd3b507f0dd1e4a688ddf8f890cf82188
evan-colorkrew <evertjannes.degraaff@colorkrew.com>
2024-06-03 17:51:29 +0900

    add prompt

--- base/src/test.py
+++ base/src/test.py
@@ -13,7 +13,14 @@ mistral_models_path = "/models/Mistral-7B-Instruct-v0.3"
 tokenizer = MistralTokenizer.from_file(f"{mistral_models_path}/tokenizer.model.v3")
 model = Transformer.from_folder(mistral_models_path)
 
-completion_request = ChatCompletionRequest(messages=[UserMessage(content="Explain Machine Learning to me in a nutshell.")])
+with open("./prompt.txt", "r") as file:
+    prompt = file.read()
+
+# get the text from the file ../transcription/test-1.txt
+with open("../transcription/test-1.txt", "r") as file:
+    userMsg = file.read()
+
+completion_request = ChatCompletionRequest(messages=[UserMessage(content=f"{prompt} \"{userMsg}\"")])
 
 tokens = tokenizer.encode_chat_completion(completion_request).tokens
 

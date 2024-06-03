3de726212d884f23cd533e5da23414f5d58b7d58
evan-colorkrew <evertjannes.degraaff@colorkrew.com>
2024-06-03 23:25:38 +0900

    change build routine

--- build.sh
+++ build.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-docker compose \
-    -f ./docker/docker-compose.yml -f ./docker/docker-compose-gpu.yml \
-    build
-docker compose \
-    -f ./docker/docker-compose.yml -f ./docker/docker-compose-gpu.yml \
-    start
-docker compose \
-    -f ./docker/docker-compose.yml -f ./docker/docker-compose-gpu.yml \
-    exec base sh /setup.sh
+docker compose -f ./docker/docker-compose.yml -f ./docker/docker-compose-gpu.yml build
+docker compose -f ./docker/docker-compose.yml -f ./docker/docker-compose-gpu.yml up -d
+docker compose -f ./docker/docker-compose.yml -f ./docker/docker-compose-gpu.yml exec base sh /setup.sh

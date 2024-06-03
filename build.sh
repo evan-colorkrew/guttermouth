791a189c950e5e58b949fd547abf6815d1638e9e
evan-colorkrew <evertjannes.degraaff@colorkrew.com>
2024-06-03 23:17:59 +0900

    test

--- build.sh
+++ build.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 docker compose \
-    -f ./docker/docker-compose.yml \
-    -f ./docker/docker-compose-gpu.yml \
+    -f ./docker/docker-compose.yml -f ./docker/docker-compose-gpu.yml \
     build
 docker compose \
-    -f ./docker/docker-compose.yml \
-    -f ./docker/docker-compose-gpu.yml \
+    -f ./docker/docker-compose.yml -f ./docker/docker-compose-gpu.yml \
     start
 docker compose \
-    -f ./docker/docker-compose.yml \
-    -f ./docker/docker-compose-gpu.yml \
+    -f ./docker/docker-compose.yml -f ./docker/docker-compose-gpu.yml \
     exec base sh /setup.sh
--- start.sh
+++ start.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 docker compose \
-    -f ./docker/docker-compose.yml \
-    -f ./docker/docker-compose-gpu.yml  \
+    -f ./docker/docker-compose.yml -f ./docker/docker-compose-gpu.yml  \
     up -d
 docker compose \
-    -f ./docker/docker-compose.yml \
-    -f ./docker/docker-compose-gpu.yml \
+    -f ./docker/docker-compose.yml -f ./docker/docker-compose-gpu.yml \
     exec base sh
